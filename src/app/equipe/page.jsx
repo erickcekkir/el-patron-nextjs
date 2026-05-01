a082c30fd54f723fce9ea2af6f970071c42f55f0
erickcekkir <erick.cekkir@gmail.com>
2026-05-01 01:30:54 +0000

    feat: insert team page basic structure

--- src/app/equipe/page.jsx
+++ src/app/equipe/page.jsx
@@ -1,3 +1,30 @@
 export default function TeamPage() {
-  return <></>;
+  return (
+    <>
+      <section className="team-page">
+        <h1>Nossa Equipe</h1>
+
+        <div className="team-container">
+          <div className="team-card">
+            <img src="https://s2.glbimg.com/DZxFltgy2T9zZRZVtbwL2P_JbuY=/e.glbimg.com/og/ed/f/original/2018/06/19/nariko.jpg" alt="Matheus Sanches" />
+            <div className="team-info"></div>
+          </div>
+
+          <div className="team-card">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReJ7ZV-txb7zAGj7VUpXT9vvThDxxdxBXikA&s" alt="Raphael Rosas" />
+            <div className="team-info"></div>
+          </div>
+
+          <div className="team-card">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReJ7ZV-txb7zAGj7VUpXT9vvThDxxdxBXikA&s" alt="Cristiano Rosa" />
+            <div className="team-info"></div>
+          </div>
+           <div className="team-card">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReJ7ZV-txb7zAGj7VUpXT9vvThDxxdxBXikA&s" alt="Ian de Morais" />
+            <div className="team-info"></div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
