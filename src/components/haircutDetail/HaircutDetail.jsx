ec6dc6ca16cb35cbb2250bb03b39a898676caf7a
ggarabs <ggarabetti0607@gmail.com>
2026-05-08 19:49:41 -0300

    add hardcutDetail component module CSS

--- src/components/haircutDetail/HaircutDetail.jsx
+++ src/components/haircutDetail/HaircutDetail.jsx
@@ -1,15 +1,16 @@
 import Image from "next/image";
 import TagList from "@/components/tagList/TagList";
 import Tag from "../tag/Tag";
+import styles from "@/components/haircutDetail/haircutDetail.module.css";
 
 export default function HaircutDetail({ haircut }) {
   return (
-    <main className="detail-page">
+    <main className={styles.detailPage}>
       <h1>{haircut.name}</h1>
 
-      <div className="detail-summary">
+      <div className={styles.detailSummary}>
         <Image
-          className="detail-img"
+          className={styles.detailImg}
           src={haircut.image}
           alt={haircut.name}
           width={500}
