6c7fe88cb420cf3940831d44d2a7502b8ab42907
joaquimprieto <joaquim.mpp@outlook.com>
2026-05-09 20:09:20 -0300

    fix: update team member images to correct file paths

--- src/lib/team.js
+++ src/lib/team.js
@@ -3,25 +3,25 @@ const team = [
     id: "matheus-sanches",
     name: "Matheus-Sanches",
     role: "Barbeiro Junior",
-    image: "/imgs/Screenshot 2026-04-02 at 21.26.09.png",
+    image: "/imgs/matheus.png",
   },
   {
     id: "raphael-rosas",
     name: "Raphael Rosas",
     role: "Especialista em Barba",
-    image: "/imgs/Screenshot 2026-04-02 at 21.26.17.png",
+    image: "/imgs/raphael.png",
   },
   {
     id: "cristiano-rosa",
     name: "Cristiano Rosa",
     role: "Barbeira Sênior",
-    image: "/imgs/Screenshot 2026-04-02 at 21.26.41.png",
+    image: "/imgs/cristiano.png",
   },
   {
     id: "ian-morais",
     name: "Ian Morais",
     role: "Aprendiz",
-    image: "/imgs/Screenshot 2026-04-02 at 21.36.14.png",
+    image: "/imgs/ian.png",
   },
 ];
 
